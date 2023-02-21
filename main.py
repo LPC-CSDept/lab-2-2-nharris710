9c098643a952968b2703821fb728e8291c69523f
nharris710 <noaharris248@gmail.com>
2023-02-21 00:04:43 -0800

    salary calculated

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    sub1 = 'Python Programing'
-    sub2 = 'Python Programing'
-    print(sub1[7:])
-    print(sub2[0:7])
-    print(sub1[7:] + sub2[0:7])
+  workhours = int(input('50 work hours'))
+reg_hours = int(40)
+o_hours = int(10)
+reg_rate = float(18.25)
+o_rate = float(27.78)
+overtime_charge = ( o_hours * o_rate)
+regular_charge = (reg_hours * reg_rate)
+print('regular_charge: ',reg_hours * reg_rate)
+print('overtime_charge: ',o_hours * o_rate)
+total_wage = regular_charge + overtime_charge
+print('total_wage: ',regular_charge + overtime_charge)
 
 
 if __name__ == '__main__':
